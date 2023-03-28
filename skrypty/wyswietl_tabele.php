b373cb65ee340a1ac549602595f2cdebd768da92
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-03-28 17:43:22 +0200

    tabele zadania

--- skrypty/wyswietl_tabele.php
+++ skrypty/wyswietl_tabele.php
@@ -16,33 +16,31 @@
        $sql = "SELECT c.id, state, c.city FROM cities c inner join states S on S.id = C.state_id";
  
        $result = $conn->query($sql);
-        
+      
        echo <<< USERSTABLE
-       <table>
-       <tr>
-          <th>Id</th>
-          <th>Wojewodztwo</td>
-          <th>Miasto</th>
-
-       </tr>
-       USERSTABLE;
+         <table border=1>
+            <tr>
+              <th>Id</th>
+              <th>Wojewodztwo</th>
+              <th>Miasto</th>
+            </tr>
+    USERSTABLE;
 
        if(!$result || mysqli_num_rows($result) == 0){
 
-         //echo '<tr><td colspan="7">brak wynikow</td></tr>';
+         echo '<tr><td colspan="7">brak wynikow</td></tr>';
 
        }
        else{
        while($user = $result->fetch_assoc()){
         echo <<< USERSTABLE
          <tr> 
-             <td>$user[id]</td>
-             <td>$user[state]</td>
-             <td>$user[city]</td>
-             <td><a href="../skrypty/delete_city.php?deleteCityId=$user[id]">Usun</a></td>
-
-         <tr>
-         USERSTABLE;   
+           <td>$user[id]</td>
+           <td>$user[state]</td>
+           <td>$user[city]</td>
+           <td><a href="../skrypty/delete_city.php?deleteCityId=$user[id]">Usun</a></td>
+         </tr>
+    USERSTABLE;   
        }
       }
 
@@ -59,6 +57,52 @@
 
        }
       }
+
+
+/*
+    require_once "../skrypty/connect.php";
+	  $sql = "SELECT c.id, state, c.city FROM cities c inner join states S on S.id = C.state_id";
+
+    $result = $conn->query($sql);
+    echo <<< USERSTABLE
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Wojewodztwo</th>
+          <th>Miasto</th>
+        </tr>
+USERSTABLE;
+
+    if ($result->num_rows > 0){
+	    while($user = $result->fetch_assoc()){
+		    echo <<< USERSTABLE
+        <tr>
+          <td>$user[id]</td>
+          <td>$user[state]</td>
+          <td>$user[city]</td>
+          <td><a href="../skrypty/delete_city.php?deleteCityId=$user[id]">Usun</a></td>
+        </tr>
+USERSTABLE;
+	    }
+    }else{
+	    echo <<< USERSTABLE
+        <tr>
+          <td colspan="19">Brak rekordów do wyświetlenia</td>
+        </tr>
+USERSTABLE;
+    }
+
+    
+    echo "</table>";
+    if (isset($_GET["deleteCity"])){
+	    if ($_GET["deleteCity"] != 0){
+		    echo "<hr>Usunięto miasto o id = $_GET[deleteCity]";
+	    }else{
+		    echo "<hr>Nie usunięto miasta";
+	    }
+    }
+*/
+
     ?>
 
    
