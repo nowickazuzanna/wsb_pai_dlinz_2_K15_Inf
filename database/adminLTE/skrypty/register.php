2a539f70e6e58f885f30a206c8518a43f62894cc
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-04-15 00:41:25 +0200

    register

--- database/adminLTE/skrypty/register.php
+++ database/adminLTE/skrypty/register.php
@@ -1,33 +1,15 @@
 
 <?php
 
-/*
+
 //echo "<pre>";
 
   //print_r($_POST);
 
 //echo "</pre>";
 
-require_once "./connect.php";
-$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
-
-//$stmt = $mysqli->prepare("INSERT INTO users VALUES (?, ?, ?, ?)");
-
-$stmt->bind_param('sissss', $_POST["email1" ], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["password1"]);
-
-$stmt->execute();
-
-echo $stmt->affected_rows;
-*/
 
 
-
-echo "<pre>";
-		print_r($_POST);
-echo "</pre>";
-
-
-/*
 	require_once "./connect.php";
 	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
 
@@ -36,7 +18,12 @@ echo "</pre>";
 	$stmt->execute();
 
 	echo $stmt->affected_rows;
-  */
+  
+    /*
+    echo "<pre>";
+		print_r($_POST);
+    echo "</pre>";*/
+
 
 
 
@@ -81,7 +68,12 @@ $stmt->bind_param("sissss", $_POST["email1"], $_POST["city_id"], $_POST["firstNa
 $stmt->execute();
 
 echo $stmt->affected_rows;
-
 */
+
+/*
+echo "<pre>";
+    print_r($_POST);
+echo "</pre>";*/
+
 
 
