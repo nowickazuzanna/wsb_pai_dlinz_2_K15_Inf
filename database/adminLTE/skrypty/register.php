04b7234416727d8d9c1826bc9fbb9eb1cd8d0731
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-04-14 23:51:30 +0200

    register

--- database/adminLTE/skrypty/register.php
+++ database/adminLTE/skrypty/register.php
@@ -1,4 +1,7 @@
+
 <?php
+
+/*
 //echo "<pre>";
 
   //print_r($_POST);
@@ -15,7 +18,70 @@ $stmt->bind_param('sissss', $_POST["email1" ], $_POST["city_id"], $_POST["firstN
 $stmt->execute();
 
 echo $stmt->affected_rows;
+*/
 
 
 
-?>
+echo "<pre>";
+		print_r($_POST);
+echo "</pre>";
+
+
+/*
+	require_once "./connect.php";
+	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
+
+	$stmt->bind_param('sissss', $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["password1"]);
+
+	$stmt->execute();
+
+	echo $stmt->affected_rows;
+  */
+
+
+
+/*
+session_start();
+$error = 0;
+
+foreach ($_POST as $key => $value) {
+    if (empty($value)) {
+        $_SESSION["error"] = "Wypełnij wszystkie pola w formularzu!";
+        $error++;
+        break;
+    }
+}
+
+if ($error == 0 && $_POST["email1"] !== $_POST["email2"]) {
+    $_SESSION["error"] = "Adres email musi być taki sam!";
+    $error++;
+}
+
+if ($error == 0 && $_POST["password1"] !== $_POST["password2"]) {
+    $_SESSION["error"] = "Hasło musi być takie samo w obu polach!";
+    $error++;
+}
+
+if ($error == 0 && !isset($_POST["terms"])) {
+    $_SESSION["error"] = "Zatwierdź regulamin!";
+    $error++;
+}
+
+if ($error != 0) {
+    echo "<script>history.back()</script>";
+    exit();
+}
+
+$hashedPassword = password_hash($_POST["password1"], PASSWORD_ARGON2I);
+
+require_once "connect.php";
+// zabezpieczenie przed sql injection
+$stmt = $conn->prepare("INSERT INTO users (email, city_id, firstName, lastName, birthday, password, created_at) VALUES (?, ?, ? ,?, ?, ?, CURRENT_TIMESTAMP());");
+$stmt->bind_param("sissss", $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $hashedPassword);
+$stmt->execute();
+
+echo $stmt->affected_rows;
+
+*/
+
+
