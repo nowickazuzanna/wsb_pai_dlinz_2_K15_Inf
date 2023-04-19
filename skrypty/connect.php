9271e04354c7a2ea67201f8a1a73c5bc10db6990
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-04-19 10:06:28 +0200

    register

--- skrypty/connect.php
+++ skrypty/connect.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli("localhost", "root", "", "test");
+  $conn = new mysqli("localhost", "root", "", "wsb_k15");
  // echo "db";
  // echo $conn->connect_errno; // komunikat o polaczaniu, jak 0 to dobrze
 
