1179c97bc8cb21eb29ba02ead9a0f1d01c571e6b
nowickazuzanna <zuzannanowicka17@gmail.com>
2023-05-15 19:31:11 +0200

    walidacja hasła i uporządkowanie

--- database/adminLTE/skrypty/login.php
+++ database/adminLTE/skrypty/login.php
@@ -1,6 +1,39 @@
 <?php
 
-/*
+if ($_SERVER["REQUEST_METHOD"] == 'POST'){
+	//print_r($_POST);
+	$errors = [];
+	foreach ($_POST as $key => $value){
+		if(empty($value)){
+			$errors[] = "Pole <b>$key</b> musi być wypełnione";
+		}
+	}
+	//print_r($errors);
+	//echo $error_message;
+
+	if(!empty($errors)){
+		$error_message = implode("<br>", $errors);
+		header("Location: ../pages/index.php?error=".urlencode($error_message));
+		exit();
+	}
+
+	echo "email: ".$_POST["email"].", hasło: ".$_POST["password"]."<br>";
+
+
+	//$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); //  j<b>an@</b>o2.pl  => jban@bo2.pl
+	//echo $email;
+
+	echo htmlentities($_POST["email"]); //j<b>an@</b>o2.pl
+
+}else{
+	header("location: ../pages");
+}
+
+
+
+
+
+/* ----v1----
 session_start();
 
 if($_SERVER["REQUEST_METHOD"] == 'POST'){
@@ -49,39 +82,7 @@ else{
 
 
 
-if ($_SERVER["REQUEST_METHOD"] == 'POST'){
-	//print_r($_POST);
-	$errors = [];
-	foreach ($_POST as $key => $value){
-		if(empty($value)){
-			$errors[] = "Pole <b>$key</b> musi być wypełnione";
-		}
-	}
-	//print_r($errors);
-	//echo $error_message;
-
-	if(!empty($errors)){
-		$error_message = implode("<br>", $errors);
-		header("Location: ../pages/index.php?error=".urlencode($error_message));
-		exit();
-	}
-
-	echo "email: ".$_POST["email"].", hasło: ".$_POST["password"]."<br>";
-
-
-	//$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); //  j<b>an@</b>o2.pl  => jban@bo2.pl
-	//echo $email;
-
-	echo htmlentities($_POST["email"]); //j<b>an@</b>o2.pl
-
-}else{
-	header("location: ../pages");
-}
-
-
-
-
-/*
+/*----v2---
 session_start();
 // sprawdzenie, czy formularz jest wysłany metodą POST
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
--- database/adminLTE/skrypty/register.php
+++ database/adminLTE/skrypty/register.php
@@ -8,24 +8,17 @@ echo "<pre>";
 echo "</pre>";
 */
 
-//1.regulamin nie zaznaczony
-//2.adres mail rozny
-//3.hasla sa rozne
-
 //dodac zmienna pomocnicza error (0), jak bedzie blad to 1 
 //jesli error != 0 to cofamy uzytkownika i wyswietlamy komunikat (zmienna sesyjna) o bledzie nad formularzem
   
 session_start();
 
-
     //$error = 0;
     foreach($_POST as $key => $value){
         if (empty($value)){
-            //echo "Wypełnij wszystkie pola";
             $_SESSION["error"] = "Wypełnij wszystkie pola";
             echo "<script>history.back();</script>";
-            //echo $_SESSION["error"];
-            exit(); //przeryw wykonywanie skryptu
+            exit(); //przerywa wykonywanie skryptu
         }
     }
 
@@ -36,12 +29,10 @@ session_start();
 		$error++;
 	}
 
-
     if (!isset($_POST["gender"])){
 		$_SESSION["error"] = "Wybierz płeć!";
 		$error++;
 	}
-
 
     if($_POST["password1"] != $_POST["password2"]){
         $error = 1;
@@ -49,103 +40,27 @@ session_start();
         $_SESSION["error"] = "Hasla sa rozne";
         //exit();
     }
-
+    
     if($_POST["email1"] != $_POST["email2"]){
         $error = 1;
-       // echo "<script>history.back();</script>";
         $_SESSION["error"] = "Adresy email sa rozne";
-        //exit();
     }
 
-    //walidacja hasła czy spelnia wymagnia
-/*
-    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["password1"])) {
-        echo "test";
-
-        $error = 1;
-        $_SESSION["error"] = "Hasło nie spełnia wymagań!";
-    }
-*/
-
-/*
-if ($error == 0 && !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["password1"])) {
-    $_SESSION["error"] = "Hasło nie spełnia wymagań!";
-    $error++;
-}
-   */
-  
-   
-
-
-
-   //walidacja hasła
+ 
+//walidacja hasła
 
 if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["password1"])) {
 	$error = 1;
 	$_SESSION["error"] = "Hasło nie spełnia wymagań!";
 }
 
-
-
-    /*
-    if($_POST["email1"] == $_POST["email1"]){
-        $error = 1;
-       // echo "<script>history.back();</script>";
-        $_SESSION["error"] = "Zduplikowane adresy mail";
-        //exit();
-    }
-    */
-
-
-    /*
-    mysqli_query('INSERT INTO ...');
-    if (mysqli_errno() == 1062) {
-    print 'duplikat!';
-    }
-    */
-
-
-
-
-
-
-
-    /*
-    //------------------------------------------------
-    require_once "./connect.php";
-    $duplicate = mysqli_query($conn, "SELECT * from users where email = $_POST[email1] ");
-    if (mysqli_num_rows($duplicate) > 0){
-        $error = 1;
-        //$duplicate = 1;
-       // echo "<script>history.back();</script>";
-        $_SESSION["error"] = "Zduplikowane adresy mail";
-        //exit();
-
-       // header("Location: index.php?message=User name or Email id already exists.");
-       //$duplicate=mysqli_query($conn,"SELECT * from 'users' where email='$email'");
-    }
-    */
-
-
-
-
-    /*
-    //mysqli_query('INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());');
-    if (mysqli_errno($conn) == 1062)
-    {
-       //print 'no way!'; 
-
-       $error = 1;
-       // echo "<script>history.back();</script>";
-        $_SESSION["error"] = "Adresy email sa zduplikowane";
-    }
-    */
-
-
-
-
-
-
+//walidacja hasła v2
+/*
+if ($error == 0 && !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["password1"])) {
+    $_SESSION["error"] = "Hasło nie spełnia wymagań!";
+    $error++;
+}
+   */
 
 
     if($error != 0 ){
@@ -153,37 +68,7 @@ if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POS
         exit();
     }
 
-
-    //mail
-    //haslo
-    //regulamin
-
-    /*
-    if (!isset($_POST["term"])){
-		$_SESSION["error"] = "Zatwierdź regulamin!";
-		$error++;
-	}*/
-
-
-
-
-
-/*
-    session_start();
-	//print_r($_POST);
-	$error = 0;
-	foreach ($_POST as $key => $value){
-		// echo "$key: $value<br>";
-		if (empty($value)){
-			// echo "$key<br>";
-			$_SESSION["error"] = "Wypełnij wszystkie pola w formularzu!";
-			echo "<script>history.back();</script>";
-			exit();
-		}
-	}
-*/
-
-
+    
     require_once "./connect.php";
 
     $sql = "SELECT * FROM users WHERE email = ? ";
@@ -197,60 +82,35 @@ if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POS
         echo "<script>history.back();</script>";
         exit();
     }
-    //header("location: ../pages/register.php");
-
-
-
-
 
 	require_once "./connect.php";
-	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `gender`, `avatar`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?,  current_timestamp());");
 
+	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `gender`, `avatar`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?,  current_timestamp());");
 
     $pass = password_hash( password: '$_POST["password1"]', algo: PASSWORD_ARGON2ID);
 
-    /*
-    if($_POST['gender'] == 'm' )
-    {
-        $avatar = 'men.jpg';
-
-    }else{
-        $avatar = 'women.jpg';
-    }
-    */
-
     $avatar = ($_POST["gender"] == 'm') ? './jpg.man.png' : './jpg.woman.png' ;
-
-    
 
 	$stmt->bind_param('sissssss', $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["gender"], $avatar, $pass);
 
 	$stmt->execute();
 
-    //$pass = password_hash( password: '$_POST["passowrd1]')
-
 	echo $stmt->affected_rows;
   
-
     if ($stmt->affected_rows == 1 ){
         $_SESSION["success"] = "Dodano uzytkownika $_POST[firstName] $_POST[lastName]";
 
-    
     }else{
         $_SESSION["error"] = "Nie udalo sie dodac rekordu ";
     }
 
-    //header("location: ../pages/register.php");
-
-    /*
-    echo "<pre>";
-		print_r($_POST);
-    echo "</pre>";*/
+    header("location: ../pages/register.php");
 
 
 
 
-/*
+
+/*---v2-------
 session_start();
 $error = 0;
 
